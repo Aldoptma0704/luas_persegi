ba65bb1d54039b8513ae2455992078d615bbce7d
Aldo Pratama <112595321+Aldoptma0704@users.noreply.github.com>
2022-09-08 09:46:35 +0700

    Add files via upload

---
+++ luas persegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s,luas;
+
+    cout<<"masukan sisi persegi : ";
+    cin>>s;
+
+    luas=s*s;
+    cout<<"luas persegi adalah "<<luas;
+    return 0 ;
+}
